78828f4249a647963baca3e8abd9e4c52d2fbcd6
colbytimm <timmcolby@hotmail.com>
2016-10-12 20:49:01 -0700

    storyboard update - background

---
+++ PokeCell.swift
@@ -0,0 +1,26 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Colby Timm on 2016-10-12.
+//  Copyright © 2016 Colby Timm. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+}
